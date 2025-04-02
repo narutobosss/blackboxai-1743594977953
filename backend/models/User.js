99f31a5feb3fdf964d7e76691c1123682d916c2e
narutobosss <lidyapratiwi7462@gmail.com>
2025-04-02 17:27:01 +0530

    Add backend/models/User.js 1743595020311

---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  wallet: {
+    type: Number,
+    default: 0
+  },
+  lotteryEntries: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Lottery'
+  }]
+});
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
